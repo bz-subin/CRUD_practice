6398aa9ffd286883b152e2b8416609a21fe49fea
김수빈 <bz.subin@gmail.com>
2025-12-27 21:19:08 +0900

    변화 규칙 지정

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-f = open("first.txt", "w") #파일 열기_파일명, 모드(W-쓰기모드)
-# python main.py로 실행해야 메모장에 내용 반영
+f = open("first.txt", "w", encoding="utf-8") #*파일 열기_파일명, 모드(W-쓰기모드), 변환규칙)
+#* python main.py로 실행해야 메모장에 내용 반영
+# encoding=utf-8을 추가해야 글자가 깨지지 않음(올바르게 변환 가능하도록 규칙 지정)
+# (UTF-8: 전 세계 모든 문자를 담은 국제 표준 사전)
 
 f.write("내용 쓰기")
 
